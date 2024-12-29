d20b78eb79f6b98be754dbc29354d08d24563640
Titusevich <nickitatitaevskiy@gmail.com>
2024-12-29 13:07:08 +0200

    Add method of volume calculation

--- src/app/Main.java
+++ src/app/Main.java
@@ -16,8 +16,17 @@ public class Main {
         int number = scan.nextInt();
         int square = square(number);
         System.out.println("The square of number " + number + " is " + square + ".");
+
+        count++;
+        radius = 4.5;
+        height = 8.5;
+        System.out.println("\n" + count +  ") The volume of cylinder with radius " + radius +
+                " and height " + height + " is " + volume(radius, height) + ".");
     }
     public static int square(int num) {
         return num * num;
+    }
+    public static double volume(double radius, double height) {
+        return Math.pow(radius, 2) * height * Math.PI;
     }
 }
