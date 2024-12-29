7f349db80197e0e5bf603209e0a576f838b0ee6d
Titusevich <nickitatitaevskiy@gmail.com>
2024-12-29 13:09:50 +0200

    Add method of calculation the elements sum

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -22,11 +23,25 @@ public class Main {
         height = 8.5;
         System.out.println("\n" + count +  ") The volume of cylinder with radius " + radius +
                 " and height " + height + " is " + volume(radius, height) + ".");
+
+        count++;
+        array = new int[]{14, 6, 21, 9, 11};
+        String strArray = Arrays.toString(array);
+        System.out.println("\n" + count + ") Array of numbers: " + strArray +
+                "\nSum of all array elements equals " + getSumma(array) + ".");
     }
+
     public static int square(int num) {
         return num * num;
     }
     public static double volume(double radius, double height) {
         return Math.pow(radius, 2) * height * Math.PI;
+    }
+    public static int getSumma(int... numbers) {
+        int summa = 0;
+        for (int num : numbers) {
+            summa += num;
+        }
+        return summa;
     }
 }
