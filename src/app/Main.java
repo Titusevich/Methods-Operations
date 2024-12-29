36b1842747f78a5baa8374967d0e9234e69c3981
Titusevich <nickitatitaevskiy@gmail.com>
2024-12-29 13:03:25 +0200

    Add method of calculation the number square

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,23 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
     static int count;
     static double radius;
     static double height;
     static int[] array;
+
+    public static void main(String[] args) {
+
+        count++;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("\n" + count + ") Enter int number:");
+        int number = scan.nextInt();
+        int square = square(number);
+        System.out.println("The square of number " + number + " is " + square + ".");
+    }
+    public static int square(int num) {
+        return num * num;
+    }
 }
